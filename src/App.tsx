00a2ea43d9e20935e2fa335f4564dc814ec5b67c
Eli Shegurov <ilyashegurov@gmail.com>
2024-03-15 16:26:48 +0200

    Dev /login commit1

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
           <div>Dev branch commit1</div>
           <div>Dev branch commit2</div>
           <div>Dev branch commit45</div>
+          <div>Dev /login commit1</div>
+          <div>Dev /login commit2</div>
+          <div>Dev /login commit2</div>
 
       </div>
   );
